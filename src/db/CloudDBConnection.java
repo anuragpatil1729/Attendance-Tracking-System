ba05d9e03324feca4aa2a0ba5ed5dc92024906cf
Anurag Patil <anurag151006@gmail.com>
2026-04-17 16:56:01 +0530

    Migrate cloud DB docs/config to Aiven MySQL

--- src/db/CloudDBConnection.java
+++ src/db/CloudDBConnection.java
@@ -5,6 +5,7 @@ import config.ConfigLoader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public final class CloudDBConnection {
     private CloudDBConnection() {
@@ -16,8 +17,36 @@ public final class CloudDBConnection {
         String dbName = ConfigLoader.get("db.name");
         String user = ConfigLoader.get("db.user");
         String pass = ConfigLoader.get("db.password");
-        String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName + "?useSSL=true&serverTimezone=UTC";
-        return DriverManager.getConnection(url, user, pass);
+        String caPath = ConfigLoader.get("db.ssl.ca");
+        boolean hasCaPath = !caPath.isEmpty();
+
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName
+                + "?useSSL=true"
+                + "&requireSSL=true"
+                + "&verifyServerCertificate=" + (hasCaPath ? "true" : "false")
+                + "&useUnicode=true"
+                + "&characterEncoding=UTF-8"
+                + "&serverTimezone=UTC"
+                + "&connectTimeout=10000"
+                + "&socketTimeout=30000";
+
+        Properties props = new Properties();
+        props.setProperty("user", user);
+        props.setProperty("password", pass);
+
+        if (hasCaPath) {
+            props.setProperty("trustCertificateKeyStoreUrl", "file:" + caPath);
+            props.setProperty("trustCertificateKeyStorePassword", "");
+        }
+
+        try {
+            return DriverManager.getConnection(url, props);
+        } catch (SQLException e) {
+            throw new SQLException(
+                    "Could not connect to Aiven MySQL. Check config.properties and ensure your IP is not blocked.",
+                    e
+            );
+        }
     }
 
     public static boolean isReachable() {
